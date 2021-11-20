743e5b3c1673300de61c86368545904606d484d3
lemonstener <deyanvasilev7@gmail.com>
2021-11-20 16:58:00 -0600

    home,about,contact done

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ const links = {
 // INITIAL
 
 function setInitial() {
-  home.innerHTML = '<p>HOME</p><i class="fas fa-home"></i>';
-  about.innerHTML = '<p>ABOUT</p><i class="fas fa-user-alt"></i>';
-  projects.innerHTML = '<i class="fas fa-folder"></i><p>PROJECTS</p>';
-  contact.innerHTML = '<i class="fas fa-phone"></i><p>CONTACT</p>';
+  home.innerHTML = '<span>HOME</span><i class="fas fa-home"></i>';
+  about.innerHTML = '<span>ABOUT</span><i class="fas fa-user-alt"></i>';
+  projects.innerHTML = '<i class="fas fa-folder"></i><span>PROJECTS</span>';
+  contact.innerHTML = '<i class="fas fa-phone"></i><span>CONTACT</span>';
   quarters.forEach((q) => {
     q.addEventListener("click", toggle, true);
   });
@@ -51,7 +51,7 @@ function toggle() {
     q.removeEventListener("click", toggle, true);
   });
   highlight();
-  // links[current].call();
+  links[current.id].call();
 }
 
 function highlight() {
@@ -59,15 +59,13 @@ function highlight() {
   wrapper.style.boxShadow = "none";
   for (let q of quarters) {
     if (q !== current) {
-      q.style.width = "0";
       q.style.height = "0";
-      q.style.opacity = "0";
       q.style.zIndex = "-1";
       q.innerHTML = "";
     } else {
       q.style.width = "100%";
       q.style.height = "100%";
-      q.style.top = "10vh";
+      q.style.top = "0";
       q.style.left = "0";
       q.style.borderRadius = "10px";
       q.innerHTML = '<i class="fas fa-times-circle close"></i>';
@@ -85,7 +83,6 @@ function back() {
   for (q of quarters) {
     q.style.width = "200px";
     q.style.height = "200px";
-    q.style.opacity = "1";
     q.style.zIndex = "1";
   }
   if (current === home) {
@@ -109,12 +106,103 @@ function back() {
   setInitial();
 }
 
-function showHome() {}
+// HOME PAGE
 
-function showAbout() {}
+function showHome() {
+  const panel = document.createElement("div");
+  panel.className = "home-panel";
 
-function showProjects() {}
+  const info = document.createElement("div");
+  info.className = "home-info";
+  const h1 = document.createElement("h1");
+  const p = document.createElement("span");
+  h1.innerText = "Deyan Vasilev";
+  p.innerHTML = "Software Engineer";
 
-function showContact() {}
+  info.append(h1, p);
+  panel.append(info);
+  home.append(panel);
+  panel.style.opacity = "1";
+}
+
+// ABOUT PAGE
+
+function showAbout() {
+  const aboutText = document.createElement("div");
+  aboutText.innerHTML = `
+  <h1>About me</h1>
+  <p>
+  Hello and thank you for visiting my website! <br> My name is Deyan, 
+  I am originally from Bulgaria and have lived in the United States since 2012. <br>
+  I have always had an interest in coding and computers and back in late 2020 I 
+  decided to pursue my passion full-time. I view coding as the ability to create 
+  fun and useful things out of thin air and I see every problem as a puzzle to be
+  cracked (I love puzzles). I love learning new technologies and am always working 
+  on improving, both as a person and as a developer. I love both Front and Back End Web Development
+  and I specialize in JavaScript.
+  I am currently looking for opportunities in the Greater St. Louis, Missouri area 
+  as well as Illinois and remote.
+  </p>
+  `;
+  aboutText.className = "about-text";
+
+  const aboutSkills = document.createElement("div");
+  aboutSkills.innerHTML = `
+  <h1>Skills</h1>
+  <p>
+  <b>Programming Languages:</b> Javascript, Python, SQL <br>
+  <b>Front End:</b> HTML, CSS, React.js, Bootstrap, jQuery <br>
+  <b>Back End:</b> Node, Express, Flask, SQLAlchemy <br>
+  <b>Other:</b> OOP, unittest, RESTful APIs
+  </p>
+  `;
+  aboutSkills.className = "about-skills";
+
+  const btn = document.createElement("div");
+  btn.innerText = "SKILLS";
+  btn.className = "about-btn";
+  btn.id = "me";
+  about.append(aboutText, btn, aboutSkills);
+
+  btn.addEventListener("click", function () {
+    switchAbout(btn.id);
+  });
+}
+
+function switchAbout(arg) {
+  const btn = document.querySelector(`#${arg}`);
+  if (arg === "me") {
+    document.querySelector(".about-text").style.left = "-1000px";
+    document.querySelector(".about-skills").style.left = "0";
+    btn.innerText = "ABOUT ME";
+    btn.id = "skills";
+  } else {
+    document.querySelector(".about-text").style.left = "0";
+    document.querySelector(".about-skills").style.left = "1000px";
+    btn.innerText = "SKILLS";
+    btn.id = "me";
+  }
+}
+
+// PROJECTS PAGE
+
+function showProjects() {
+  console.log("projects");
+}
+
+// CONTACT PAGE
+
+function showContact() {
+  const panel = document.createElement("div");
+
+  panel.innerHTML = `
+  <h3>Feel free to get in touch with me!</h4>
+  <a target="_blank" href="https://www.linkedin.com/in/deyan-vasilev/"><i class="fab fa-linkedin linkedin"></i></a>
+  <a target="_blank" href="https://github.com/lemonstener"><i class="fab fa-github github"></i></a>
+  <a target="_blank" href="mailto: deyanvasilev7@gmail.com"><i class="fas fa-envelope mail"></i></a>
+  `;
+
+  contact.append(panel);
+}
 
 setInitial();
